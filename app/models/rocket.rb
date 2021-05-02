679a270db49c7d1eac30433a038f20ac13ebc153
BigRichi <big.richi@outlook.com>
2021-05-01 23:46:28 -0400

    graphs

--- app/models/rocket.rb
+++ app/models/rocket.rb
@@ -2,5 +2,24 @@ class Rocket < ApplicationRecord
     has_many :launches
     has_many :launchpads, through: :launches
 
+    def total_launches 
+        launches.size
+    end
     
+    def successful_launches    
+        launches.where(success: true).size
+    end
+    
+    def unsuccessful_launches    
+        launches.where(success: false).size
+    end
+
+    def successful_cost
+        self.cost_per_launch * self.successful_launches
+    end
+
+    def unsuccessful_cost
+        self.cost_per_launch * self.unsuccessful_launches
+    end
+
 end
--- app/serializers/rocket_serializer.rb
+++ app/serializers/rocket_serializer.rb
@@ -1,4 +1,4 @@
 class RocketSerializer < ActiveModel::Serializer
-    attributes :id, :height, :diameter, :mass, :flickr_images, :name, :active, :cost_per_launch, :success_rate_pct, :first_flight, :wikipedia, :description
+    attributes :id, :height, :diameter, :mass, :flickr_images, :name, :active, :cost_per_launch, :success_rate_pct, :first_flight, :wikipedia, :description , :successful_launches, :successful_cost,:unsuccessful_launches, :unsuccessful_cost
     has_many :launches
 end 
