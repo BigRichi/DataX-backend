3871316db735b3a5594a9cedc2d9490321ecc042
BigRichi <big.richi@outlook.com>
2021-05-04 13:39:17 -0400

    tuesday

--- app/controllers/launch_reviews_controller.rb
+++ app/controllers/launch_reviews_controller.rb
@@ -12,7 +12,10 @@ class LaunchReviewsController < ApplicationController
 
     def create
         new_review = LaunchReview.create(launch_id: params[:launch_id], reviewer: params[:reviewer], present_at_launch: params[:present_at_launch],comment: params[:comment])
-        render json: new_review
+
+        launch = launchReview.find(params[:launch_id])
+
+        render json: [{new_review, launch}]
     end 
 
 end
